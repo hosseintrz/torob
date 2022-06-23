b1d1da63009d596e090c0fa2c2ba7f95c8249054
hosseintrz <htrzit@gmail.com>
2022-06-23 20:59:36 +0430

    Api-gateway & auth grpc-client

---
+++ rest/internal/gateway/auth_client.go
@@ -0,0 +1,51 @@
+package gateway
+
+import (
+	"context"
+	pb "github.com/hosseintrz/torob/rest/pb/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+type AuthClient struct {
+	Client pb.AuthClient
+}
+
+func InitAuthClient(url string) *AuthClient {
+	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Couldn't connect to auth service: %s\n", err)
+	}
+	client := &AuthClient{
+		Client: pb.NewAuthClient(conn),
+	}
+	return client
+}
+
+func (c *AuthClient) Signup(fullName, email, username, password string, role int32) (*pb.AuthResponse, error) {
+	req := &pb.SignupRequest{
+		Fullname: fullName,
+		Email:    email,
+		Username: username,
+		Password: password,
+		Role:     pb.SignupRequest_Role(role),
+	}
+	res, err := c.Client.Signup(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *AuthClient) Login(username, password string) (*pb.AuthResponse, error) {
+	req := &pb.LoginRequest{
+		Username: username,
+		Password: password,
+	}
+	res, err := c.Client.Login(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
